8a9ba9bfb6cf9703469c625b31ea1c43ed73d34c
Chris Burghdurf <chrisburghdurf@Chriss-MacBook-Air-2.local>
2026-01-30 22:30:53 -0500

    Add hero background image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
 import type { AppProps } from "next/app";
 import "leaflet/dist/leaflet.css";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-
-
